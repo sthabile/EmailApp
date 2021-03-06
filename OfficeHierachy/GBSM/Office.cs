0a41d8cfc4f8169659a2290f824611964f4d2a27
Sthabile Lushaba <sthabile@tj.local>
2021-03-06 20:16:34 +0200

    initial commit

---
+++ OfficeHierachy/GBSM/Office.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GBSM
+{
+    public class Office : IEnumerable<Office>
+    {
+        public string Name; // id to identify each office 
+        public Office Parent; // to keep track of the office to which this office reporst to
+        public int Amount = 0;
+        int Total = 0;
+        private readonly Dictionary<string, Office> Childern = new Dictionary<string, Office>(); //all offices that report to this office
+
+        /// <summary>
+        /// constructor
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="amount"></param>
+        public Office(string name)
+        {
+            this.Name = name;
+        }
+        public Office()
+        {
+
+        }
+
+        public void SetAmount(int amount)
+        {
+            this.Amount = amount;
+
+        }
+        /// <summary>
+        /// returns a office that descends from it
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public Office GetDescendant(string name)
+        {
+            return this.Childern[name];
+        }
+
+        /// <summary>
+        /// Adds a new descendant to the current office e.g SA office to HeadOffice
+        /// </summary>
+        /// <param name="NewOffice"></param>
+        public void Add(Office NewOffice)
+        {
+            NewOffice.Parent = this;
+            this.Childern.Add(NewOffice.Name, NewOffice);
+
+        }
+
+        /// <summary>
+        /// Calculates the Net Profit of an Office by adding up the profits of all descending offices
+        /// </summary>
+        /// <returns></returns>
+        public int NetProfit()
+        {
+            Total = Amount;
+            if (this.Childern.Count() == 0)
+            {
+                return Total;             //stop case. Net Profit is the Amount unless the office has descendants
+            }
+
+            foreach (var office in Childern)
+            {
+                Total += office.Value.NetProfit();
+            }
+            return Total;
+        }
+        public IEnumerator<Office> GetEnumerator()
+        {
+            IEnumerator<Office> IEnum = this.Childern.Values.GetEnumerator();
+            return IEnum;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
---
+++ OfficeHierachy/GBSM/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections;
+
+namespace GBSM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            try //use trycatch in case the file isn't found 
+            {
+                //read the excel file line by line into a string array 
+                Console.WriteLine("Enter input File Name");
+                string[] input = File.ReadAllLines(Console.ReadLine());
+                Dictionary<string, Office> AllOffices = new Dictionary<string, Office>(); //Possible parents
+
+                #region //use the OFFICE class to sort and store the data
+                for (int line = 1; line < input.Length; line++) //read each line from the given file
+                {
+                    string[] temp = input[line].Split(',');    // split at commas 
+                    /*create currOffice using first item in temp
+                      *use the first item to see if parent office is null.
+                      *create an office using the second item and set curr.parent to it
+                     */
+                    Office currOffice = new Office(temp[0]);
+                    currOffice.SetAmount(Convert.ToInt32(temp[2]));
+                    if (currOffice.Name != "HeadOffice")
+                    {
+
+                        Office ParentOffice;
+                        if (AllOffices.TryGetValue(temp[1], out ParentOffice))
+                        {
+                            ParentOffice.Add(currOffice);
+                        }
+                    }
+                    AllOffices.Add(currOffice.Name, currOffice);  //Add to list of all offices
+
+                }
+
+                #endregion
+
+
+                #region  //read in input and display the Net Profit
+                while (true)
+                {
+                    Console.Write("Question 1 \nEnter office to calculate Nett profit for:\n");
+                    string currOfficeName = Console.ReadLine();
+                    try
+                    { 
+                        //use the input string to index AllOffices and get the Net Profit.
+                        Console.WriteLine(AllOffices[currOfficeName].NetProfit() + "\n");
+                        Console.WriteLine("....Press Enter/C to exit/continue....\n");
+                        if (string.IsNullOrEmpty(Console.ReadLine()))
+                        {
+                            break;
+                        }
+                        else { continue; }
+
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Invalid Office name.\n");
+                        continue;
+                    }
+
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Could not find file");
+                Console.ReadLine();
+            }
+
+
+        }
+            #endregion
+
+
+
+
+        
+    }
+}
